414dbdafee4835aae767b51a7ad8b2a3b91df7ee
creatijin <newlex0124@gmail.com>
2020-11-24 03:22:41 +0900

    머터리얼 앱 기본 구조

---
+++ lib/simple_app.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Hello Flutter Creatijin',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello Fluter Creatijin')),
+        body: Text('Hello Fluter Creatijin'),
+      ),
+    ));
